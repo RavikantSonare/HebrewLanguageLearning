06a8427b896463b1603644a89dc1dee76ed6ab2e
RavikantSonare <ravikantsonare@gmail.com>
2017-12-06 20:26:55 +0530

    sysnc

--- HebrewLanguageLearning_Users/ViewModels/Dashboard/DashboardViewModel.cs
+++ HebrewLanguageLearning_Users/ViewModels/Dashboard/DashboardViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace HebrewLanguageLearning_Users.ViewModels.Dashboard
 
@@ -15,10 +18,39 @@ namespace HebrewLanguageLearning_Users.ViewModels.Dashboard
         //{
 
         //}
+        private string _bibleTxt = @"בראשית ברא אלוהים את השמים ואת הארץ.
+והארץ היתה ללא צורה, וריק; ואת החושך על פני המעמקים.ורוח אלוהים נעה על פני המים.
+ויאמר אלהים יהי אור ויהי אור.
+וירא ה 'את האור, כי טוב: ואלוהים חילק את האור מהחושך.
+ואלוהים קרא את יום האור, ואת החושך הוא קרא לילה.והערב והבוקר היו היום הראשון.
+ויאמר אלהים, יהי ברקע בתוך המים, ונתן לו לחלק את המים מן המים.";
+   //     private ItemsControl _progressBox;
+   //     public ItemsControl ProgressBox
+   //     {
+   //         get { return _progressBox; }
+   //         set
+   //         {
+   //             _bibleTxt.ItemsSource = Enumerable.Range(1, 60)
+   //.Select(r => new KeyValuePair<string, string>(r.ToString(), r.ToString())).ToList(); ;
+   //             NotifyOfPropertyChange(() => ProgressBox);
+   //         }
+   //     }
+        public string BibleTxt
+        {
+            get { return _bibleTxt; }
+            set
+            {
+                _bibleTxt = value;
+                NotifyOfPropertyChange(() => BibleTxt);
+            }
+        }
         protected override void OnActivate()
         {
             base.OnActivate();
+
             //MessageBox.Show("Deshboard");//This is for testing - currently doesn't display
         }
+
+
     }
 }
--- HebrewLanguageLearning_Users/Views/Dashboard/DashboardView.xaml.cs
+++ HebrewLanguageLearning_Users/Views/Dashboard/DashboardView.xaml.cs
@@ -23,10 +23,43 @@ namespace HebrewLanguageLearning_Users.Views.Dashboard
         public DashboardView()
         {
             InitializeComponent();
+
             //UIElement parent = App.Current.MainWindow;
             //parent.IsEnabled = true;
 
+            var numberButtons = Enumerable.Range(1, 50).Select(r => new rectelgleProp(r < 5 ? "#00D5B6" : "#C3C3C3", r.ToString(), (r + 9) % 10 == 0 ? r.ToString() : "",
+                r == 5 ? null : "", r == 6 ? Convert.ToString(r * 100) + " 0 0 0" : Convert.ToString((r > 10 ? r % 10 : r) * 32 + (r > 6 ? 38 : 0)) + " " + Convert.ToString(r / 10 > 0 ? r / 10 * 50 : 0)
+                + " " + "0 0", r == 5 ? 100 : 30)).ToList();
+            numberButtonItems.ItemsSource = numberButtons;
 
+            //var numberButtons = Enumerable.Range(1, 50).Select(r => new KeyValuePair<string, string>(r.ToString(), (r + 9) % 10 == 0 ? r.ToString() : "")).ToList();
+            //numberButtonItems.ItemsSource = numberButtons;
         }
+
+       
     }
+    public class rectelgleProp
+    {
+        public string ColorProp { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string rectType { get; set; }
+        public string margin { get; set; }
+        public int width { get; set; }
+
+
+        public rectelgleProp(string v1, string v2, string v3, string v4, string v5, int v6)
+        {
+            this.ColorProp = v2 == "5" ? null : v1;
+            this.Key = v2;
+            this.Value = v3;
+            this.rectType = v4;
+            this.margin = v5;
+            this.width = v2 == "5" ? 100 : v6;
+        }
+
+
+    }
+
+
 }
